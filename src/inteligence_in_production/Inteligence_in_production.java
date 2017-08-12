2332d56029e4089f5d8e6006b48bac2469c0ac42
Artur <arturh@viavale.com.br>
2017-08-11 23:50:38 -0300

    Iniciando

---
+++ src/inteligence_in_production/Inteligence_in_production.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inteligence_in_production;
+
+import Views.JanelaPrincipal;
+
+/**
+ *
+ * @author Ghost
+ */
+
+public class Inteligence_in_production {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaPrincipal window = new JanelaPrincipal();
+        window.setExtendedState(window.getExtendedState() | window.MAXIMIZED_BOTH);
+        window.setVisible(true);
+    }
+    
+}
